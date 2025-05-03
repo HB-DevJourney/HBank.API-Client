7ab2b102eb158892ac9e1c3dd0071a1e6dfbc4f3
Mateus <tech.mateussilva@gmail.com>
2025-05-03 15:44:17 -0300

    Projeto inicial: API Cliente

---
+++ src/test/java/com/hbank/cliente/ClienteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hbank.cliente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClienteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
